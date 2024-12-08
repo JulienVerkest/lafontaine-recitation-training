924626efa29287fa74abf708e1f1359e40768f1d
Julien Verkest <julienverkest@gmail.com>
2024-12-08 08:46:38 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/lafontaine-recitation-training/",
   plugins: [react()],
   server: {
     port: 3003, // You can change this to any port you prefer
